85fa530b21925ca12f9f072d620c8913ae12adfa
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-16 18:46:46 +0700

    create user set up

--- backend-golang/controllers/user_controller.go
+++ backend-golang/controllers/user_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"septianadipratama/backend-api/database"
+	"septianadipratama/backend-api/helpers"
 	"septianadipratama/backend-api/models"
 	"septianadipratama/backend-api/structs"
 
@@ -23,4 +24,53 @@ func FindUsers(c *gin.Context) {
 		Message: "Lists Data Users",
 		Data:    users,
 	})
+}
+
+func CreateUser(c *gin.Context) {
+
+	//struct user request
+	var req = structs.UserCreateRequest{}
+
+	// Bind JSON request ke struct UserRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusUnprocessableEntity, structs.ErrorResponse{
+			Success: false,
+			Message: "Validation Errors",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	// Inisialisasi user baru
+	user := models.User{
+		Name:     req.Name,
+		Username: req.Username,
+		Email:    req.Email,
+		Password: helpers.HashPassword(req.Password),
+	}
+
+	// Simpan user ke database
+	if err := database.DB.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "Failed to create user",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	// Kirimkan response sukses
+	c.JSON(http.StatusCreated, structs.SuccessResponse{
+		Success: true,
+		Message: "User created successfully",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+
 }
