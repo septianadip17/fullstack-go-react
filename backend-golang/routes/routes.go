1b9234730ce772883d93dc014c3be915b47435e5
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-16 18:40:34 +0700

    login success

--- backend-golang/routes/routes.go
+++ backend-golang/routes/routes.go
@@ -14,5 +14,8 @@ func SetupRouter() *gin.Engine {
 	// route register
 	router.POST("/api/register", controllers.Register)
 
+	// route login
+	router.POST("/api/login", controllers.Login)
+
 	return router
 }
