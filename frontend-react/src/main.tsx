5ec8f2885394dcc561ec20602cbeb745689906cf
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-19 22:29:46 +0700

    hook register and context added

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,14 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
-//import BrowserRouter dari react router
-import { BrowserRouter } from 'react-router';
+//import BrowserRouter from react router
+import { BrowserRouter } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+//import QueryClient dan QueryClientProvider from react-query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+//init QueryClient
+const queryClient = new QueryClient();
+
+//import AuthProvider
+import { AuthProvider } from "./context/AuthContext";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <AuthProvider>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </AuthProvider>
+  </StrictMode>
+);
