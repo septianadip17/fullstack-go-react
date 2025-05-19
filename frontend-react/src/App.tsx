02314bb544ca44b656d957fd21a323bdc2dacf55
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-19 22:50:47 +0700

    success clickable

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import FC from react
+import { FC } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+//import router
+import AppRoutes from "./routes";
 
+//import Link from react router
+import { Link } from "react-router";
+
+const App: FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <nav className="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div className="container">
+          <Link to="/" className="navbar-brand">
+            HOME
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a
+                  href="https://santrikoding.com"
+                  target="_blank"
+                  rel="noopener"
+                  className="nav-link active"
+                  aria-current="page"
+                >
+                  SANTRIKODING.COM
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-export default App
+      <div className="container mt-5">
+        <AppRoutes />
+      </div>
+    </div>
+  );
+};
+
+export default App;
