9fd4c6ca09d272a79506e715388c6665694e7a68
Septian Adi Pratama <septianadip7998@gmail.com>
2025-05-19 12:26:19 +0700

    main tsx changed and api tsx added

--- frontend-react/src/main.tsx
+++ frontend-react/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+//import BrowserRouter dari react router
+import { BrowserRouter } from 'react-router';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 )
